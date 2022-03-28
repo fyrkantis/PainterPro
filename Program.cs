01ca275b28c2c8367519120524880e0112612637
fyrkantis <david@kniberg.com>
2022-03-28 20:25:13 +0200

    Fixed Scriban templates and started adding post request handling.

--- Program.cs
+++ Program.cs
@@ -1,38 +1,88 @@
-﻿using System.Net;
+﻿using Scriban;
+using Scriban.Parsing;
+using Scriban.Runtime;
+using System.Net;
 using System.Text;
 
 namespace PainterPro
 {
 	public static class PainterPro
 	{
+		public static string currentDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
 		public static async Task HandleConnectionAsync(this HttpListenerContext context)
 		{
 			HttpListenerRequest request = context.Request;
 			HttpListenerResponse response = context.Response;
 			Stream output = response.OutputStream;
 
-			if (request.RawUrl == null)
+			
+
+			if (request.Url == null)
 			{
-				response.SendError(400, "Bad request", "No requested URL was specified.");
+				response.SendError(400, "Bad Request", "No requested URL was specified.");
+				return;
+			}
+			if (request.Url.LocalPath == "/")
+			{
+				response.SendRedirect("/index.html", 308, "Permanent Redirect");
 				return;
 			}
 
-			string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Website\\", request.RawUrl.Replace('/', '\\').TrimStart('\\'));
+			if (request.HttpMethod.ToLower() == "post")
+			{
+				HandlePost(request);
+			}
+
+			string path = Path.Combine(currentDirectory, "Website\\", request.Url.LocalPath.Replace('/', '\\').TrimStart('\\'));
 			if (!File.Exists(path))
 			{
-				response.SendError(404, "Not found", "The requested file '" + request.RawUrl + "' could not be found.");
+				response.SendError(404, "Not Found", "The requested file '" + request.RawUrl + "' could not be found.");
 				return;
 			}
-			Console.Write(" Sending file '" + path + "'");
+			
 			response.ContentType = MimeTypes.GetMimeType(path);
-			response.ContentLength64 = new FileInfo(path).Length;
 			response.AddHeader("Content-Disposition", "inline; filename = \"" + Path.GetFileName(path) + "\"");
-			using (Stream fileStream = File.OpenRead(path))
+			Console.Write(response.ContentType);
+			if (!response.ContentType.EndsWith("html"))
 			{
-				fileStream.CopyTo(output);
+				Console.Write(" Sending file '" + path + "'");
+				response.ContentLength64 = new FileInfo(path).Length;
+				using (Stream fileStream = File.OpenRead(path))
+				{
+					fileStream.CopyTo(output);
+				}
 			}
+			else
+			{
+				Console.Write(" Converting");
+				ScriptObject script = new ScriptObject(); // Used for sending arguments to html template.
+				script.Add("thing", "Hello World!");
+				TemplateContext templateContext = new TemplateContext();
+				templateContext.TemplateLoader = new MyTemplateLoader();
+				templateContext.PushGlobal(script);
+
+				Template template = Template.Parse(File.ReadAllText(path, Encoding.UTF8));
+				byte[] data = Encoding.UTF8.GetBytes(template.Render(templateContext));
+
+				response.ContentLength64 = data.Length;
+				output.Write(data);
+			}
+			
 			output.Close();
+			response.Close();
 			Console.WriteLine(" Done!");
+		}
+
+		public static string HandlePost(HttpListenerRequest request)
+		{
+			Console.WriteLine(" Reading post:");
+			string text;
+			using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+			{
+				text = reader.ReadToEnd();
+			}
+			Console.WriteLine(text);
+			return "Idk?";
 		}
 
 		public static void SendError(this HttpListenerResponse response, int code, string message, string body = "")
@@ -48,6 +98,34 @@ namespace PainterPro
 			Stream output = response.OutputStream;
 			output.Write(bytes);
 			output.Close();
+			response.Close();
+		}
+
+		public static void SendRedirect(this HttpListenerResponse response, string location, int code, string message = "")
+		{
+			response.StatusCode = code;
+			response.StatusDescription = message;
+			response.RedirectLocation = location;
+			response.OutputStream.Close();
+			response.Close();
+		}
+
+		public class MyTemplateLoader : ITemplateLoader
+		{
+			public string GetPath(TemplateContext context, SourceSpan callerSpan, string templateName) // TODO: Adapt for relative paths.
+			{
+				return Path.Combine(currentDirectory, "Website\\", templateName.Replace('/', '\\').TrimStart('\\'));
+			}
+
+			public string Load(TemplateContext context, SourceSpan callerSpan, string templatePath)
+			{
+				return File.ReadAllText(templatePath, Encoding.UTF8);
+			}
+
+			public ValueTask<string> LoadAsync(TemplateContext context, SourceSpan callerSpan, string templatePath)
+			{
+				throw new NotImplementedException();
+			}
 		}
 	}
 
